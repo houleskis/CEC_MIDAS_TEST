7c30102d607051c1cbf2f027d8b1c00d61059c52
houleskis <72902408+houleskis@users.noreply.github.com>
2021-07-22 16:08:01 -0400

    Add files via upload

---
+++ MIDAS_GetPrice.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+#Query passes in JWT access token as "token", Rate Identification Number (RIN) as rateID, and "alltime" or "realtime" for queryType
+
+def GetPrice(token, rateID, queryType):
+    headers = {'accept': 'application/json', 'Authorization': "Bearer " + token}
+    url = 'https://cecwats2.org/api/pricedata?id=' + rateID + '&querytype=' + QueryType
+    pricing_response = requests.get(url, headers=headers)
+
+    return (json.loads(pricing_response.text))
