1b4dd038e9614874bbaa02f032d886f57005a436
houleskis <72902408+houleskis@users.noreply.github.com>
2021-07-26 09:23:27 -0400

    Update MIDAS_Token.py

--- MIDAS_Token.py
+++ MIDAS_Token.py
@@ -5,11 +5,10 @@ import base64
 #Function passes through your MIDAS username and password as strings and returns the JWT token as a string
 
 def GetToken(username, password):
-    #encodes the username and password as bytes
-    user_encodedBytes = base64.b64encode(username.encode("utf-8"))
-    pswd_encodedBytes = base64.b64encode(password.encode("utf-8"))
-
-    headers = {b'Authorization': b'BASIC ' + user_encodedBytes + b':' + pswd_encodedBytes}
+    credentials = username + ":" + password
+    credentials_encodedBytes = base64.b64encode(credentials.encode("utf-8"))
+   
+    headers = {b'Authorization': b'BASIC ' + credentials_encodedBytes}
     url = 'https://cecwats2.org/api/token'
 
     response = requests.get(url,headers=headers)
